312be932e42698a82cedce6bb0098442e973eaac
Huy Nguyen <hqnguyen1998@gmail.com>
2022-02-06 01:25:04 -0500

    Edit api url

--- libs/getApiUrl.js
+++ libs/getApiUrl.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-const apiUrl = dev ? 'http://localhost:3000' : process.env.PRODUCTION_URL;
+const apiUrl = dev ? 'http://localhost:3000' : process.env.VERCEL_URL;
 
 export default apiUrl;
