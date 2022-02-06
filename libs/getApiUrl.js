4997e45db80a3adcb4d9607001a40b65a9dd3f97
Huy Nguyen <hqnguyen1998@gmail.com>
2022-02-06 01:58:29 -0500

    Edit api url

--- libs/getApiUrl.js
+++ libs/getApiUrl.js
@@ -1,7 +1,5 @@
-const dev = process.env.VERCEL_ENV === 'production';
+const dev = process.env.VERCEL_ENV !== 'production';
 
-const apiUrl = dev
-  ? 'http://localhost:3000'
-  : 'https://ecommerce-next-app-three.vercel.app';
+const apiUrl = dev ? 'http://localhost:3000' : process.env.API_URL;
 
 export default apiUrl;
