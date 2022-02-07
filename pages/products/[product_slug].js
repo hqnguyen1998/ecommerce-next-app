42ce60fc3351aad84f6080486914162d08be92c1
Huy Nguyen <hqnguyen1998@gmail.com>
2022-02-06 20:06:48 -0500

    Add Fetch product detail api

--- pages/products/[product_slug].js
+++ pages/products/[product_slug].js
@@ -3,43 +3,42 @@ import fetch from 'isomorphic-unfetch';
 import apiUrl from '../../libs/getApiUrl';
 import LayoutComponent from '../../components/layout/layout';
 
-const SingleProductPage = () => {
+const SingleProductPage = ({ product: { data } }) => {
   return (
     <LayoutComponent pageTitle='Product page'>
-      {/* {data.product_title} */}
-      page details
+      {data.product_title}
     </LayoutComponent>
   );
 };
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch(`${apiUrl}/api/product`, {
-//     method: 'GET',
-//   });
+export const getStaticPaths = async () => {
+  const res = await fetch(`${apiUrl}/api/product`, {
+    method: 'GET',
+  });
 
-//   const products = await res.json();
+  const products = await res.json();
 
-//   const paths = await products.data.map((product) => ({
-//     params: { product_slug: product.slug },
-//   }));
-//   return {
-//     paths: paths,
-//     fallback: false,
-//   };
-// };
+  const paths = await products.data.map((product) => ({
+    params: { product_slug: product.slug },
+  }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
 
-// export const getStaticProps = async ({ params }) => {
-//   const res = await fetch(`${apiUrl}/api/product/${params.product_slug}`, {
-//     method: 'GET',
-//   });
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`${apiUrl}/api/product/${params.product_slug}`, {
+    method: 'GET',
+  });
 
-//   const product = await res.json();
+  const product = await res.json();
 
-//   return {
-//     props: {
-//       product: product.data,
-//     },
-//   };
-// };
+  return {
+    props: {
+      product: product,
+    },
+  };
+};
 
 export default SingleProductPage;
